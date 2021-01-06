a848d6c1e64bc2fba2ed2d350172331df19568e0
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-01-06 13:18:04 +0900

    Update osver.h

--- 1.2 beta/tcom/osver.h
+++ 1.2 beta/tcom/osver.h
@@ -3,6 +3,18 @@
 #define OS_TERCOM_VER 1.00
 
 void osver(){
-      	 os_print_string("AhnTri Version:");
-         print_int(OS_VER);
+	os_print_color("                     ", BLACK, BLACK);
+	os_print_color("        /\\         OS: AhnTri 1.20       " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("       /  \\        Host: Seek on 5       " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("      /    \\       Kernel: AhnTri Kernel " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("     / beta \\      Terminal: NumDOS      " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("    /  1.20  \\     Themes: AhnTri        " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("   / AhnTri 1 \\                          " , BLACK, CYAN);
+	os_print_color("\n                     ", BLACK, BLACK);
+	os_print_color("  /____________\\                         " , BLACK, CYAN);
 }

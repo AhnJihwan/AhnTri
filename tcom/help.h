c567feefae80f5291aae01b3e3ca3b64e3f9cb73
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-02-18 21:00:24 +0900

    Every function will be changed

--- tcom/help.h
+++
@@ -1,4 +0,0 @@
-void help(){
-  os_print_string("\nHELP for this help, CCALC for C calculator, OSVER for os version, clscr for clear screen, exit for exiting, cputest for CPU testing, notes for NoteTaker, pedx for edx, atroid for ahntroid, homenu for GeX.\n\n");
-}
-

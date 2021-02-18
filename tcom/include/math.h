79cf07d614d342e48fc20c0ba62b67c994fd492a
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-02-18 21:09:07 +0900

    Update math.h

--- tcom/include/math.h
+++ tcom/include/math.h
@@ -1,10 +1,10 @@
 // Define what ppl use a lot
-#define Math_PI = 3.14159265359;
+#define Math_PI = 3.14159265359;					//Stable...
 #define ROOT_TWO = 1.414213562373095048801688724209698079;
 
 // Powering 2, 3, 4
 int powt(int a){
-    return a * a;
+    return a * a;							//Better for functions which does not only works in ATOS.
 }
 
 int powth(int a){
@@ -28,7 +28,7 @@ int divf(int a, int b){
     }
 }
 
-int addnpth(int a, int b){
+int addnpth(int a, int b){						//This is for (a+b)^3
     int m = a * a * a;
     int n = b * b * b;
     int mp = 3*a*a*b;

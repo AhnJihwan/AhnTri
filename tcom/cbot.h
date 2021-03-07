796e3a0667948a75898381561a101500917686a9
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-03-07 12:26:33 +0900

    Add files via upload

--- tcom/cbot.h
+++ tcom/cbot.h
@@ -9,23 +9,23 @@ void ahntroid(){
 	input = read_char();
 	if(strcmp(input, "hello")==0 || strcmp(input, "hola")==0 || strcmp(input, "hi")==0 || strcmp(input, "hi man")==0 || strcmp(input, "dude")==0){
 		printf("Hello, User Master. This AI is well-trained by arrays of words or numbers");
-		printf("\nbut have only one output. Whatever, good day!");
-		ahntroid();
+		printf("\nbut have only one output. Whatever, have a good day!");
+		free(input);
 	}else if(strcmp(input, "bye")==0 || strcmp(input, "good bye")==0 || strcmp(input, "bye")==0){
 		printf("Bye-Bye, Good-Bye!!");
 		kernmain();
 	}else if(strcmp(input, "help")==0){
 		printf("Help: exit() to exit");
-		ahntroid();
+		free(input);
 	}else if(strcmp(input, "wow")==0 || strcmp(input, "whow")==0 || strcmp(input, "omg")==0 || strcmp(input, "lol")==0){
 		printf("Why, is AhnTriOS too good? If is, give a *heart in Github!");
-		ahntroid();
+		free(input);
 	}else if(strcmp(input, "happi")==0 || strcmp(input, "happy")==0 || strcmp(input, "good")==0 || strcmp(input, "great")==0 || strcmp(input, "thanks")==0){
 		printf("I am happy to hear that you are happy!");
-		ahntroid();
+		free(input);
 	}else if(strcmp(input, "fu")==0 || strcmp(input, "fuck")==0 || strcmp(input, "fuck u")==0 || strcmp(input, "fuck you")==0 || strcmp(input, "crap")==0 || strcmp(input, "shit")==0 || strcmp(input, "jesus")==0){
 		printf("Don't say those words. If you do, fuck you :(");
-		ahntroid();
+		free(input);
 	}else{
 		printf("Word not trained or not exist in code.");
 		
--- tcom/mem.h
+++ tcom/mem.h
@@ -1,16 +1,17 @@
 char args[];
 	
 void memanset(){
-	char *inpLoc = args[0];
+	int *inpLoc = args[0];
 	char *inpDat = args[1];
 	char *value = ((char*)stoi(inpLoc));
+	printf("One alphabet to store: ");
 	inpDat = read_char();
 	memcpy(value, inpDat, sizeof(char));
 	printf("OK!");
 }
 
 void memprintf(){
-	char *inpLoc = args[0];
+	int *inpLoc = args[0];
 	char *value = ((char*)stoi(inpLoc));
 	printf(value);
 }

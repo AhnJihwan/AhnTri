ce532d37aaa115792e01bec220b328a01202692d
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-03-15 14:13:13 +0900

    Add files via upload

---
+++ AhnLibc/advmath.h
@@ -0,0 +1,31 @@
+//Advanced Calculator
+
+int kb2mb(int kb){
+	int mb = kb*1024;
+	return mb;
+}
+
+int mb2gb(int mb){
+	int gb = mb*1024;
+	return gb;
+}
+
+int kg2lb(int kg){
+	int lb = kg*2;
+	return lb;
+}
+
+int lb2kg(int lb){
+	int kg = lb/2;
+	return kg;
+}
+
+int in2cm(int in){
+	int cm = in*3;
+	return cm;
+}
+
+int cm2in(int cm){
+	int in = cm/3;
+	return in;
+}

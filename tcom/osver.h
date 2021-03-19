ecc20df04ecf023f0a62f26f2165ce3077661d00
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-03-19 11:29:20 +0900

    Update osver.h

--- tcom/osver.h
+++ tcom/osver.h
@@ -27,13 +27,13 @@ Terminal name       OS_NUM
 */
 
 void osver(){
-	os_print_color("        /\\       OS: AhnTri OS 1.2.3.2 i386        " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n       /  \\      Machine: " , BRIGHT_BLUE, CYAN);
+	os_print_color("        /\\       OS: AhnTri OS 1.2.4 i386           " , BLACK, CYAN);
+	os_print_color("\n       /  \\      Machine: " , BLACK, CYAN);
 	bitidentify();
-	os_print_color("                   " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n      /    \\     Kernel: AhnTri 1.20 +             " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n     /      \\    Terminal: Terminal 123            " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n    /        \\   Themes: AhnTri                    " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n   /  AhnTri  \\                                    " , BRIGHT_BLUE, CYAN);
-	os_print_color("\n  /____________\\                                   " , BRIGHT_BLUE, CYAN);
+	os_print_color("                   " , BLACK, CYAN);
+	os_print_color("\n      /    \\     Kernel: AhnTri 1.20 +             " , BLACK, CYAN);
+	os_print_color("\n     /      \\    Terminal: Terminal 124            " , BLACK, CYAN);
+	os_print_color("\n    /        \\   Themes: AhnTri                    " , BLACK, CYAN);
+	os_print_color("\n   /  AhnTri  \\                                    " , BLACK, CYAN);
+	os_print_color("\n  /____________\\                                   " , BLACK, CYAN);
 }

3955842373bd4e3b4f9becb6e3f37463e3110399
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-04-11 12:19:10 +0900

    Update note.h

--- tcom/note.h
+++ tcom/note.h
@@ -1,3 +1,21 @@
+void notetake()
+{
+  char ch = 0;
+  char inp = 0;
+  do{
+    inp = get_input_keycode();
+    if(keycode == KEY_ENTER){
+    	printf("\n");
+     } else if{
+     	clscr();
+     	kernmain();
+    }else{
+      ch = get_ascii_char(keycode);
+      print_char(ch);
+    }
+  }while(ch > 0);
+}
+
 void display_help()
 {
   gotoxy(25, 0);
@@ -8,7 +26,7 @@ void display_help()
   os_print_color("\n                           ", BLACK, BLACK);
   os_print_color("|    AhnTri NoteTaker    | |", WHITE, BRIGHT_BLUE);
   os_print_color("\n                           ", BLACK, BLACK);
-  os_print_color("|-~=-~=~--v1.2-~=~--~=~--| |", WHITE, BRIGHT_BLUE);
+  os_print_color("|-~=-~=~--v1.3-~=~--~=~--| |", WHITE, BRIGHT_BLUE);
   os_print_color("\n                           ", BLACK, BLACK);
   os_print_color("| Take notes and do not  | |", WHITE, BRIGHT_BLUE);
   os_print_color("\n                           ", BLACK, BLACK);
@@ -16,11 +34,12 @@ void display_help()
   os_print_color("\n                           ", BLACK, BLACK);
   os_print_color("-------------------------|/ ", WHITE, BRIGHT_BLUE);
 }
-int NoTe;
+
 void notes(){
 	display_help();
-	print_new_line();
-	printf("                           -> ");
-	NoTe = read_int();
+	printf("\nStarting Notetaker");
+	suspend(100);
+	clscr();
+	printf("\n");
+	notetake();
 }
-

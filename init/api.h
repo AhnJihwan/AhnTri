a249a15351b70c094c21a1ec0c3609a29a52b0a8
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-04-15 02:29:14 +0000

    Add files via upload

---
+++ init/api.h
@@ -0,0 +1,8 @@
+extern void print_char(char ch);
+extern void printf(char *str);
+extern void init_vga(uint8 fg_color, uint8 bg_color);
+extern void printf(char *str);
+extern void os_print_color(char *str, uint8 fore_color, uint8 back_color);
+extern int read_int();
+extern void suspend(uint32 timer_count);
+extern int get_input_keycode();

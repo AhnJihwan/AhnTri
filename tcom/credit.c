616b9919de972063c73d3f38cd10e225de220c47
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-04-17 18:06:01 +0900

    Update credit.c

--- tcom/credit.c
+++ tcom/credit.c
@@ -1,20 +1,20 @@
 void credits(){
-	printf("============Developers==============");
-	printf("Creator: Jihwan Ahn(I am KOREAN guy)");
-	printf("=====Additional thanks to ...=======");
-	printf("BK Ahn(Dad gave me some money)");
-	printf("Ethan Recendez(friend in US who made the first Fish Dictionary together(2017))");
-	printf("u/fir4rfin for README.md typo");
-	printf("============Stargazers==============");
-	printf("parsa011, He lives in Iran");
-	printf("Riahiamirreza Joined on Aug 26, 2019");
-	printf("BOBBYWY Joined on Apr 2, 2015");
-	printf("unsafecast Joined on Dec 18, 2020");
-	printf("ehalferty who lives in San Francisco, CA");
-	printf("dreamos82 who lives in Dublin");
-	printf("programmeruser2 who lives in /dev/tty");
-	printf("0xdeit who lives in Mexico");
-	printf("growlnx who lives in Rio de Janeiro, Brazil");
-	printf("AhnJihwan who lives in Seoul, South Korea");
+	printf("============Developers==============\n");
+	printf("Creator: Jihwan Ahn(I am KOREAN guy)\n");
+	printf("=====Additional thanks to ...=======\n");
+	printf("BK Ahn(Dad gave me some money)\n");
+	printf("Ethan Recendez(friend in US who made the first Fish Dictionary together(2017))\n");
+	printf("u/fir4rfin for README.md typo\n");
+	printf("============Stargazers==============\n");
+	printf("parsa011, He lives in Iran\n");
+	printf("Riahiamirreza Joined on Aug 26, 2019\n");
+	printf("BOBBYWY Joined on Apr 2, 2015\n");
+	printf("unsafecast Joined on Dec 18, 2020\n");
+	printf("ehalferty who lives in San Francisco, CA\n");
+	printf("dreamos82 who lives in Dublin\n");
+	printf("programmeruser2 who lives in /dev/tty\n");
+	printf("0xdeit who lives in Mexico\n");
+	printf("growlnx who lives in Rio de Janeiro, Brazil\n");
+	printf("AhnJihwan who lives in Seoul, South Korea\n");
 	printf("Thank you for all, who helped me XD!");
 }

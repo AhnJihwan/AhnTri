55fd489811062c7fcb7987f7758317297c400ec8
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-04-17 08:39:58 +0000

    Add files via upload

---
+++ tcom/credit.c
@@ -0,0 +1,20 @@
+void credits(){
+	printf("============Developers==============");
+	printf("Creator: Jihwan Ahn(I am KOREAN guy)");
+	printf("=====Additional thanks to ...=======");
+	printf("BK Ahn(Dad gave me some money)");
+	printf("Ethan Recendez(friend in US who made the first Fish Dictionary together(2017))");
+	printf("u/fir4rfin for README.md typo");
+	printf("============Stargazers==============");
+	printf("parsa011, He lives in Iran");
+	printf("Riahiamirreza Joined on Aug 26, 2019");
+	printf("BOBBYWY Joined on Apr 2, 2015");
+	printf("unsafecast Joined on Dec 18, 2020");
+	printf("ehalferty who lives in San Francisco, CA");
+	printf("dreamos82 who lives in Dublin");
+	printf("programmeruser2 who lives in /dev/tty");
+	printf("0xdeit who lives in Mexico");
+	printf("growlnx who lives in Rio de Janeiro, Brazil");
+	printf("AhnJihwan who lives in Seoul, South Korea");
+	printf("Thank you for all, who helped me XD!");
+}

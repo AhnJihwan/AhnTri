483c50a695e21c252bdb601fb50e93d10bc2823d
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-04-24 12:24:39 +0900

    Update cbot.c

--- tcom/cbot.c
+++ tcom/cbot.c
@@ -36,6 +36,9 @@ void ahntroid(){
 		}else if(strcmp(input, "who are u")==0 || strcmp(input, "what is your gender")==0 || strcmp(input, "whoareu")==0 || strcmp(input, "uname")==0){
 			printf("I am AhnTroid. I am an man bot. I am Artificial Intellegence.");
 			free(input);
+		}else if(strcmp(input, "open ccalc")==0 || srtcmp(input, "plz ccalc")==0{
+			ccalc();
+			free(input);
 		}else{
 			printf("Word not trained or not exist in code.");
 			

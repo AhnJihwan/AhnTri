3c018d3ee215f8c2ef16d7c524e007147d69aacf
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-09 00:57:17 +0900

    Update types.h

--- kio/types.h
+++ kio/types.h
@@ -4,6 +4,9 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
+typedef unsigned char u8int;
+typedef unsigned short u16int;
+typedef unsigned int u32int;
 typedef void process;
 typedef int function;
 

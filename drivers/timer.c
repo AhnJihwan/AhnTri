8a9f0daf3a8fe38b069d620acabc66d7f33b1893
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-11 15:07:08 +0900

    Update timer.c

--- drivers/timer.c
+++ drivers/timer.c
@@ -1,1 +1,28 @@
+#include "../init/api.h"
+
 //My PIT driver code
+
+//Function that interrupts interrupts. Literally.
+void cli(){
+  asm volatile("cli");                                 //Thank you, u/Bob-The-One!
+}
+
+unsigned read_pit_count(void) {
+	unsigned count = 0;
+	cli();
+	outb(0x43,0b0000000);
+ 
+	count = inb(0x40);		// Low byte
+	count |= inb(0x40)<<8;		// High byte
+ 
+	return count;
+}
+
+void set_pit_count(unsigned count) {
+	// Interrupts interrupts
+	cli();
+	// Set low byte
+	outb(0x40,count&0xFF);		// Low byte
+	outb(0x40,(count&0xFF00)>>8);	// High byte
+	return;
+}

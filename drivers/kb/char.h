8c51775c2f8f9739ff9895eb5d20164ddebf7160
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-11 08:44:49 +0000

    Add files via upload

---
+++ drivers/kb/char.h
@@ -0,0 +1,12 @@
+#ifndef CHAR_H
+#define CHAR_H
+
+#include "../../kio/types.h"
+#include "keyboard.h"
+
+
+extern char get_ascii_char(uint8);
+
+#endif
+
+
---
+++ drivers/kb/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned char u8int;
+typedef unsigned short u16int;
+typedef unsigned int u32int;
+typedef void process;
+typedef int function;
+
+#endif
+

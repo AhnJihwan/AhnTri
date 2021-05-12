cd0c4e876635022e8202f464f1eabc9d38ad190e
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-12 15:09:14 +0900

    Update api.h

--- init/api.h
+++ init/api.h
@@ -7,5 +7,5 @@ extern void os_print_color(char *str, uint8 fore_color, uint8 back_color);
 extern int read_int();
 extern void suspend(uint32 timer_count);
 extern int get_input_keycode();
-extern void inb(uint16 port);
+extern uint8 inb(uint16 port);
 extern int read_uint();

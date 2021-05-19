d43f18fd7d739c1841f7c3afbeddc924f2efae13
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-19 15:11:50 +0900

    Update irq.s

--- arch/i386/irq.s
+++ arch/i386/irq.s
@@ -15,107 +15,133 @@
 .global irq14
 .global irq15
 
+.global irq0handler
+.global irq1handler
+.global irq2handler
+.global irq3handler
+.global irq4handler
+.global irq5handler
+.global irq6handler
+.global irq7handler
+.global irq8handler
+.global irq9handler
+.global irq10handler
+.global irq11handler
+.global irq12handler
+.global irq13handler
+.global irq14handler
+.global irq15handler
+ 
+extern irq0handler
+extern irq1handler
+extern irq2handler
+extern irq3handler
+extern irq4handler
+extern irq5handler
+extern irq6handler
+extern irq7handler
+extern irq8handler
+extern irq9handler
+extern irq10handler
+extern irq11handler
+extern irq12handler
+extern irq13handler
+extern irq14handler
+extern irq15handler
+ 
 irq0:
   pusha
-  outb 0x20, 0x20
+  call irq0handler
   popa
   iret
  
 irq1:
   pusha
-  outb 0x20, 0x20
+  call irq1handler
   popa
   iret
  
 irq2:
   pusha
-  outb 0x20, 0x20
+  call irq2handler
   popa
   iret
  
 irq3:
   pusha
-  outb 0x20, 0x20
+  call irq3handler
   popa
   iret
  
 irq4:
   pusha
-  outb 0x20, 0x20
+  call irq4handler
   popa
   iret
  
 irq5:
   pusha
-  outb 0x20, 0x20
+  call irq5handler
   popa
   iret
  
 irq6:
   pusha
-  outb 0x20, 0x20
+  call irq6handler
   popa
   iret
  
 irq7:
   pusha
-  outb 0x20, 0x20
+  call irq7handler
   popa
   iret
  
 irq8:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq8handler
   popa
   iret
  
 irq9:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq9handler
   popa
   iret
  
 irq10:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq10handler
   popa
   iret
  
 irq11:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq11handler
   popa
   iret
  
 irq12:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq12handler
   popa
   iret
  
 irq13:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq13handler
   popa
   iret
  
 irq14:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq14handler
   popa
   iret
  
 irq15:
   pusha
-  outb 0x20, 0xA0
-  outb 0x20, 0x20
+  call irq15handler
   popa
   iret
  

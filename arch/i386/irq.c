c5b69da75d813ce2b5d0e71a14bbd1eb0c69ff38
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-19 15:12:08 +0900

    Update irq.c

--- arch/i386/irq.c
+++ arch/i386/irq.c
@@ -10,3 +10,75 @@ void remap_pic(){
 	outb(0x21, 0);
 	outb(0xA1, 0);
 }
+
+void irq0handler(void) {
+          outb(32, 32);
+}
+ 
+void irq1handler(void) {
+	  outb(32, 32);
+}
+ 
+void irq2handler(void) {
+          outb(32, 32);
+}
+ 
+void irq3handler(void) {
+          outb(32, 32);
+}
+ 
+void irq4handler(void) {
+          outb(32, 32);
+}
+ 
+void irq5handler(void) {
+          outb(32, 32);
+}
+ 
+void irq6handler(void) {
+          outb(32, 32);
+}
+ 
+void irq7handler(void) {
+          outb(32, 32);
+}
+ 
+void irq8handler(void) {
+          outb(160, 32);
+          outb(32, 32);          
+}
+ 
+void irq9handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq10handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq11handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq12handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq13handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq14handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}
+ 
+void irq15handler(void) {
+          outb(160, 32);
+          outb(32, 32);
+}

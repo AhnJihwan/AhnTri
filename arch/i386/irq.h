b143aa56c80c3d7787fe74a8327738362c7d9d9a
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-19 16:27:06 +0900

    Create irq.h

---
+++ arch/i386/irq.h
@@ -0,0 +1,16 @@
+#define IRQ0 32
+#define IRQI 33
+#define IRQII 34
+#define IRQIII 35
+#define IRQIV 36
+#define IRQV 37
+#define IRQVI 38
+#define IRQVII 39
+#define IRQVIII 40
+#define IRQIX 41
+#define IRQX 42
+#define IRQXI 43
+#define IRQXII 44
+#define IRQXII 45
+#define IRQXIV 46
+#define IRQXV 47

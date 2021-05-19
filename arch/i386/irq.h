e89a870139c951454f0b71e1933fed256e9545ea
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-19 16:30:43 +0900

    Update irq.h

--- arch/i386/irq.h
+++ arch/i386/irq.h
@@ -14,3 +14,20 @@
 #define IRQXII 45
 #define IRQXIV 46
 #define IRQXV 47
+
+extern irq0
+extern irq1
+extern irq2
+extern irq3
+extern irq4
+extern irq5
+extern irq6
+extern irq7
+extern irq8
+extern irq9
+extern irq10
+extern irq11
+extern irq12
+extern irq13
+extern irq14
+extern irq15

3a02020d639687c638d95509ec218cab415d72cd
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-22 00:03:33 +0900

    Update timer.c

--- drivers/timer.c
+++ drivers/timer.c
@@ -1,22 +1,13 @@
 #include "../init/api.h"
+#include "../arch/i386/isr.h"
+#include "../arch/i386/irq.h"
 
-//My PIT driver code 
-unsigned read_pit_count(void) {
-	unsigned count = 0;
-	cli();
-	outb(0x43,0b0000000);
- 
-	count = inb(0x40);		// Low byte
-	count |= inb(0x40)<<8;		// High byte
- 
-	return count;
+volatile uint32 tick = 0;
+
+void pit_driver(registers *reg){
+    (void)reg;
+    tick++;											//Ticks
 }
-
-void set_pit_count(unsigned count) {
-	// Interrupts interrupts
-	cli();
-	// Set low byte
-	outb(0x40,count&0xFF);		// Low byte
-	outb(0x40,(count&0xFF00)>>8);	// High byte
-	return;
+void pbt(){
+	return tick;									//PIT based timer
 }

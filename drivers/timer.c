7e06e8a9dbf623586ce37b7cd483ca376c7c9e7f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-22 11:43:59 +0900

    Update timer.c

--- drivers/timer.c
+++ drivers/timer.c
@@ -2,6 +2,26 @@
 #include "../arch/i386/isr.h"
 #include "../arch/i386/irq.h"
 
+unsigned read_pit_count(void) {
+	unsigned count = 0;
+	cli();
+	outb(0x43,0b0000000);
+ 
+	count = inb(0x40);		// Low byte
+	count |= inb(0x40)<<8;		// High byte
+ 
+	return count;
+}
+
+void set_pit_count(unsigned count) {
+	// Interrupts interrupts
+	cli();
+	// Set low byte
+	outb(0x40,count&0xFF);		// Low byte
+	outb(0x40,(count&0xFF00)>>8);	// High byte
+	return;
+}
+
 volatile int tick = 0;
 
 void pit_driver(registers_t *reg){

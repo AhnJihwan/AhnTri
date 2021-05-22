6b7bc335819d7d09e0384549f7913a95b4ad68b9
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-05-22 17:11:08 +0900

    Update irq.h

--- arch/i386/irq.h
+++ arch/i386/irq.h
@@ -15,6 +15,14 @@
 #define IRQXIV 46
 #define IRQXV 47
 
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
+    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+};
+
 extern void irq0();
 extern void irq1();
 extern void irq2();

3825e59c1007095b0fb8a14c344e8c1e67673622
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-02 00:19:13 +0900

    Create serial.c

---
+++ drivers/serial.c
@@ -0,0 +1,24 @@
+#include "../kinc/serial.h"
+//This code was borrowed from https://github.com/dreamos82/Dreamos64/blob/master/src/kernel/io/serial.c
+int init_serial(int port){
+   outb(port + 1, 0x00);    // Disable all interrupts
+   outb(port + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+   outb(port + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+   outb(port + 1, 0x00);    //                  (hi byte)
+   outb(port + 3, 0x03);    // 8 bits, no parity, one stop bit
+   outb(port + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+   outb(port + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+   outb(port + 4, 0x1E);    // Set in loopback mode, test the serial chip
+   outb(port + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
+
+   // Check if serial is faulty (i.e: not same byte as sent)
+   if(inb(port + 0) != 0xAE) {
+      return 1;
+   }
+
+   // If serial is not faulty set it in normal operation mode
+   // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+   outb(port + 4, 0x0F);
+   return 0;
+}
+//End of Borrowing

6a36dfa2127c978c9faf1d558864ea6b538202c0
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-01 23:51:36 +0900

    Create qemu.c

---
+++ drivers/qemu.c
@@ -0,0 +1,20 @@
+#include "../kinc/qemu.h"
+#include "../kinc/serial.h"
+#include "../kinc/api.h"
+
+uint32 qemu_init_debug(){
+    init_serial(QEMU_SERIAL_PORT);
+    return 1;
+}
+
+void qemu_printf(char ch){
+    while((inb(QEMU_SERIAL_PORT + 5) & 0x20) == 0);
+    outb(QEMU_SERIAL_PORT, ch);
+}
+
+void qemu_printf_string(char *msg){
+    while (*msg != '\0'){
+        qemu_printf(*msg);
+        msg++;
+    }
+}

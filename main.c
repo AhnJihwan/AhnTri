3ad1df1111a0607b62a1a45bfb506f7b4a70a94f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-20 17:55:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -140,6 +140,7 @@ void kernmain(){
 void mkern_main(multiboot_info_t* multiboot)
 {
   if (CHECK_FLAG (multiboot->flags, 12)){
+    suspend(1);
     qemu_printf_string("Framebuffer flags(12) checked! \\/");
   }
   suspend(2);

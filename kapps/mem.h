c2a12172f622319c6436467434365b805a3ca9db
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-20 18:00:13 +0900

    Update mem.h

--- kapps/mem.h
+++ kapps/mem.h
@@ -1,17 +1,5 @@
 void memanset(){
-	char args[];
-	int *inpLoc = args[0];
-	char *inpDat = args[1];
-	char *value = ((char*)stoi(inpLoc));
-	printf("One alphabet to store: ");
-	inpDat = read_char();
-	memcpy(value, inpDat, sizeof(char));
-	printf("OK!");
 }
 
 void memprintf(){
-	char args[];
-	int *inpLoc = args[0];
-	char *value = ((char*)stoi(inpLoc));
-	printf(value);
 }

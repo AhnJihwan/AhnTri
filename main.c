4eb2f5972e3ec04ce6ad9dfd391d8cfda22fe304
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-22 23:17:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -136,91 +136,8 @@ void kernmain(){
 
 #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
 
-void draw(multiboot_info_t *mbi){
-  if (CHECK_FLAG (mbi->flags, 12))
-    {
-      multiboot_uint32_t color;
-      unsigned i;
-      void *fb = (void *) (unsigned long) mbi->framebuffer_addr;
+multiboot_info_t framebuffer_type = 1;
 
-      switch (mbi->framebuffer_type)
-        {
-        case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
-          {
-            unsigned best_distance, distance;
-            struct multiboot_color *palette;
-            
-            palette = (struct multiboot_color *) mbi->framebuffer_palette_addr;
-
-            color = 0;
-            best_distance = 4*256*256;
-            
-            for (i = 0; i < mbi->framebuffer_palette_num_colors; i++)
-              {
-                distance = (0xff - palette[i].blue) * (0xff - palette[i].blue)
-                  + palette[i].red * palette[i].red
-                  + palette[i].green * palette[i].green;
-                if (distance < best_distance)
-                  {
-                    color = i;
-                    best_distance = distance;
-                  }
-              }
-          }
-          break;
-
-        case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
-          color = ((1 << mbi->framebuffer_blue_mask_size) - 1) 
-            << mbi->framebuffer_green_field_position;
-          break;
-
-        case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
-          color = '\\' | 0x0100;
-          break;
-
-        default:
-          color = 0xffffffff;
-          break;
-        }
-      for (i = 0; i < mbi->framebuffer_width
-             && i < mbi->framebuffer_height; i++)
-        {
-          switch (mbi->framebuffer_bpp)
-            {
-            case 8:
-              {
-                multiboot_uint8_t *pixel = fb + mbi->framebuffer_pitch * i + i;
-                *pixel = color;
-              }
-              break;
-            case 15:
-            case 16:
-              {
-                multiboot_uint16_t *pixel
-                  = fb + mbi->framebuffer_pitch * i + 2 * i;
-                *pixel = color;
-              }
-              break;
-            case 24:
-              {
-                multiboot_uint32_t *pixel
-                  = fb + mbi->framebuffer_pitch * i + 3 * i;
-                *pixel = (color & 0xffffff) | (*pixel & 0xff000000);
-              }
-              break;
-
-            case 32:
-              {
-                multiboot_uint32_t *pixel
-                  = fb + mbi->framebuffer_pitch * i + 4 * i;
-                *pixel = color;
-              }
-              break;
-            }
-        }
-    }
-
-}    
 void mkern_main(multiboot_info_t* multiboot)
 {
   if (CHECK_FLAG (multiboot->flags, 12)){
@@ -233,5 +150,4 @@ void mkern_main(multiboot_info_t* multiboot)
   asm volatile("\tint $0");
   irq_install();
   qemu_printf_string("Everything is initialized. System is starting...");
-  draw(multiboot);
 }

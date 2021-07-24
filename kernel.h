11015966429c6774199cf59f4079f0975ba709ea
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-24 22:59:59 +0900

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -10,25 +10,6 @@
 
 uint16* vga_buffer;
 
-enum vga_color {
-    BLACK,
-    BLUE,
-    GREEN,
-    CYAN,
-    RED,
-    MAGENTA,
-    BROWN,
-    GREY,
-    DARK_GREY,
-    BRIGHT_BLUE,
-    BRIGHT_GREEN,
-    BRIGHT_CYAN,
-    BRIGHT_RED,
-    BRIGHT_MAGENTA,
-    YELLOW,
-    WHITE,
-};
-
 #define BLACK            0x000000
 #define BLUE             0x013571
 #define GREEN            0x015229

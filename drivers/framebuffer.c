253cb462664eedd5b7745654b555f2aa308029a1
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-24 18:23:40 +0900

    Update framebuffer.c

--- drivers/framebuffer.c
+++ drivers/framebuffer.c
@@ -10,9 +10,13 @@ multiboot_uint32_t framebuffer_pitch;
 multiboot_uint32_t framebuffer_height;
 multiboot_uint32_t framebuffer_width;
 
+uint32_t cur_x = 0;
+uint32_t cur_y = 0;
+
 //Define Macros
 #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
-
+#define FONT_HEIGHT 8
+#define FONT_WIDTH 8
 int framebuffer_check(multiboot_info_t* multiboot){
   int framebuffer_type;
   if (CHECK_FLAG (multiboot->flags, 12)){
@@ -41,4 +45,18 @@ int init_framebuffer(multiboot_info_t* mbi){
 
 void framebuffer_putpixel(uint32_t x, uint32_t y, uint32_t color){  /*this plots a single pixel to the given coordinates with the given color*/
     *(uint32_t*)(x + y * framebuffer_width + framebuffer_buffer) = color;
+}
+
+void framebuffer_putchar(char ch, uint32_t color){
+  uint8_t xx, yy;
+  if(ch=="\n"|cur_x>=framebuffer_width/FONT_WIDTH){
+    cur_x=0;
+    cru_y+=1;
+  }else{
+    for(xx=0;xx<8;xx++){
+      for(yy=0;yy<8;yy++){
+        framebuffer_putpixel(xx + cur_x*FONT_WIDTH, yy + cur_y*FONT_HEIGHT, color);
+      }
+    }
+  }
 }

2caeba0c8ba4630d50abde95bf73bbbbca31ea07
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-30 19:21:16 +0900

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -21,8 +21,8 @@ uint16* vga_buffer;
 #define DARK_GREY        0x8f8f8f
 #define BRIGHT_BLUE      0x65abe7
 #define BRIGHT_GREEN     0xa3bd8d
-#define BRIGHT_CYAN      0x00ffff
-#define BRIGHT_RED       0xff5555
+#define BRIGHT_CYAN      0x55ffff
+#define BRIGHT_RED       0xe61e2b
 #define BRIGHT_MAGENTA   0xba9da2
 #define YELLOW           0xeaa442
 #define WHITE            0xffffff

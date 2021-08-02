d72295186c1f6bddff03d4e556d7d821b22a587f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-02 22:02:24 +0900

    Create scicalc.c

---
+++ kapps/scicalc.c
@@ -0,0 +1,37 @@
+//SCIENCE CALCULATOR
+//CHEMISTRY | PYSICS
+//MADE FOR MY REAL-LIFE SCIENCE SKILL PRACTICE AS WELL AS I DID FOR CCALC-MATH AND FDIC(NEW, THE OLD ONES AREN'T FOR MY REAL-LIFE)-ENGLISH
+//COPYRIGHT 2021 JIHWAN AHN
+//AHNTRI SCIENCE CALCULATOR IS A PART OF AHNTRI OS AND IS ALSO PROTECTED BY APACHE 2.0 LICENCE AS WELL AS OTHER CHUNKS OF CODE.
+#define H 1
+#define He 2
+#define Li 3
+#define Be 4
+#define B 5
+#define C 6
+
+typedef struct chem{
+  int h;
+  int he;
+  int li;
+  int be;
+  int b;
+  int c;
+  int n;
+  int o;
+  int f;
+  int ne;
+  int na;
+  int mg;
+  int al;
+  int si;
+  int p;
+  int s;
+  int cl;
+  int ar;
+  int k;
+  int ca;
+} chem_t;
+
+void chem(){
+  chem_t h = 1;

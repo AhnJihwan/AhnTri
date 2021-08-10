88882ac81ad3959b76b1c74add43404e7ca08150
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-10 23:53:40 +0900

    Update pmm.c

--- mm/memory.c
+++
@@ -1,2 +0,0 @@
-/*Memory managements will be developed by core developer Jihwan Ahn after test*/
-/*Other developers can develop here by contributing.*/
---
+++ mm/pmm.c
@@ -0,0 +1,8 @@
+/*
+Physical memory manager by Jihwan Ahn
+Protected by Apache licence 2.0.
+Copyright 2021 Jihwan Ahn
+*/
+
+#define BLOCK_SIZE 4096
+#define KB2BLOCK(x) ((x) / 4)

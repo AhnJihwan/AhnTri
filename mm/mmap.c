aa243b534b6af4840074482a311548f1c5186e91
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-11 11:25:21 +0900

    Create mmap.c

---
+++ mm/mmap.c
@@ -0,0 +1,6 @@
+void printf_mmap_addr(multiboot_info_t *mbi, uint32_t mmap_printf, uint32_t end_mmap){
+  if(CHECK_FLAG (mbi->flags, 6)){
+    printf("memory map adress: 0x");
+    print_int((int) mbi->mmap_adrr);
+  }
+}

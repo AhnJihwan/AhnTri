efd44c16cc9f971bc0f969eb1c2c0434907b22af
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-15 21:23:54 +0900

    Update bitmap.h

--- libc/mem/bitmap.h
+++ libc/mem/bitmap.h
@@ -1,10 +1,10 @@
 /*
 bitmap array:
-          0	1	2	3	4	5	6	7 8
+                    0	1	2	3	4	5	6	7 8
 bitmap[]	1	1	1	1	1	1	1	1 1
                 |  bitmap_set(bitmap, 0);
                 V
-          0	1	2	3	4	5	6	7 8
+                    0	1	2	3	4	5	6	7 8
 bitmap[]	0	0	0	0	0	0	0	0 0
 */
 

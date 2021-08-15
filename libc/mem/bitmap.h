98abb67c68a5c6ef7403194eba971648ce0e265f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-15 21:22:59 +0900

    Create bitmap.h

---
+++ libc/mem/bitmap.h
@@ -0,0 +1,15 @@
+/*
+bitmap array:
+          0	1	2	3	4	5	6	7 8
+bitmap[]	1	1	1	1	1	1	1	1 1
+                |  bitmap_set(bitmap, 0);
+                V
+          0	1	2	3	4	5	6	7 8
+bitmap[]	0	0	0	0	0	0	0	0 0
+*/
+
+#define BITMAP_ROW_SIZE 32
+
+void bitmap_set(uint32_t *bitmap, int bit) {
+    bitmap[bit / BITMAP_ROW_SIZE] |= (1 << (bit % BITMAP_ROW_SIZE));
+}

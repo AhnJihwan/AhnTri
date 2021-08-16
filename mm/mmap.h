643199b305be4cf7a9c69bb9091e68ff44eaeef4
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-16 23:45:57 +0900

    Update mmap.h

--- mm/mmap.h
+++ mm/mmap.h
@@ -1,5 +1,5 @@
 #define CHECK_FLAG_MMAP(flags,bit)   ((flags) & (1 << (bit)))
-void printf_mmap_addr(multiboot_info_t *mbi, uint32_t mmap_printf, uint32_t end_mmap){
+void printf_mmap_addr(multiboot_info_t *mbi){
   if(CHECK_FLAG_MMAP (mbi->flags, 6)){
     printf("memory map adress: 0x");
     print_int((int) mbi->mmap_addr);

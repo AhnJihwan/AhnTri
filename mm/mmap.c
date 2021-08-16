9fffbaeda567e784f667fac84c45acb486c84631
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-16 23:36:33 +0900

    Update mmap.c

--- mm/mmap.c
+++ mm/mmap.c
@@ -1,3 +1,6 @@
+#include "../libc/atclib.h"
+#include "pmm.h"
+
 void printf_mmap_addr(multiboot_info_t *mbi, uint32_t mmap_printf, uint32_t end_mmap){
   if(CHECK_FLAG (mbi->flags, 6)){
     printf("memory map adress: 0x");

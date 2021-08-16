0ab65a17cca65e932a0c07eeb9caadd791b15dbd
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-16 23:28:51 +0900

    Update pmm.c

--- mm/pmm.c
+++ mm/pmm.c
@@ -5,16 +5,11 @@ Copyright 2021 Jihwan Ahn
 */
 
 #include "../libc/atclib.h"
+#include "pmm.h"
 
 #define BPD 32                  //Blocks Per Dword
 #define BLOCK_SIZE 4096
 #define KB2BLOCK(x) ((x) / 4)
-
-static uint8_t   mem_size;    //Available memory in kilobytes
-static uint32_t used_blocks;  //How much blocks are used/4096 blocks?
-static uint32_t max_blocks;   //Maximum of used blocks/4096
-static uint32_t *pmmap;       //Pysical memory map contaning state of the bitmap 0(free) or 1(used) of each block.
-static uint8_t   pmmap_size;  //The size of the bitmap
 
 void pmm_init(uint32_t pmmap_addr, uint8_t size) {
     mem_size    = size;                                   //Set memory size as the size which is inputted.

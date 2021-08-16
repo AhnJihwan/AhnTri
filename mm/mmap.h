a919c4597510e8411db77fe798350d7e68a8294f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-16 23:38:34 +0900

    Update and rename mmap.c to mmap.h

--- mm/mmap.h
+++ mm/mmap.h
@@ -1,5 +1,3 @@
-#include "../libc/atclib.h"
-
 void printf_mmap_addr(multiboot_info_t *mbi, uint32_t mmap_printf, uint32_t end_mmap){
   if(CHECK_FLAG (mbi->flags, 6)){
     printf("memory map adress: 0x");

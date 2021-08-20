acfa3fd02804a4ad8a5439990d3e33d29d7352c4
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-20 14:43:44 +0900

    Update cmos.c

--- drivers/cmos.c
+++ drivers/cmos.c
@@ -6,6 +6,30 @@
 /*
 From OSDev.org
 */
+int century_register = 0x00;                                // Set by ACPI table parsing code if possible
+ 
+unsigned char second;
+unsigned char minute;
+unsigned char hour;
+unsigned char day;
+unsigned char month;
+unsigned int year;
+ 
+enum {
+      cmos_address = 0x70,
+      cmos_data    = 0x71
+};
+ 
+int get_update_in_progress_flag() {
+      outb(cmos_address, 0x0A);
+      return (inb(cmos_data) & 0x80);
+}
+ 
+unsigned char get_RTC_register(int reg) {
+      outb(cmos_address, reg);
+      return inb(cmos_data);
+}
+ 
 void read_rtc() {
       unsigned char century;
       unsigned char last_second;

8817c267de4a85ff34d72b3ef667713502a7fb92
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-27 19:51:17 +0900

    Update paging.c

--- mm/paging.c
+++ mm/paging.c
@@ -1,4 +1,5 @@
 #include "paging.h"
+#include "../libc/atclib.h"
 
 page_dir_t page_directory[1024] __attribute__((aligned(4096)));
 
@@ -6,5 +7,14 @@ void clear_page_directory(uint32_t page_dir){
   int i;
   for(i = 0; i < 1024; i++) {
     page_dir[i] = 0x00000002;
+  }
+}
+
+page_table_t page_table[1024] __attribute__((aligned(4096)));
+
+void clear_page_table(uint32_t page_table){
+  uint32 i;
+  for(i = 0; i < 1024; i++){
+    first_page_table[i] = (i * 0x1000) | 3;
   }
 }

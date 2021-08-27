cc625024b66aa084765de067acdc7dc01df0c2f3
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-08-27 15:18:57 +0900

    Update paging.c

--- mm/paging.c
+++ mm/paging.c
@@ -1,1 +1,10 @@
+#include "paging.h"
 
+page_dir_t page_directory[1024] __attribute__((aligned(4096)));
+
+void clear_page_directory(uint32_t page_dir){
+  int i;
+  for(i = 0; i < 1024; i++) {
+    page_dir[i] = 0x00000002;
+  }
+}

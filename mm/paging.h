61c9f35b91a13f709e4051f9a99089be3706ba7a
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-09-01 12:46:03 +0900

    Update paging.h

--- mm/paging.h
+++ mm/paging.h
@@ -1,2 +1,23 @@
-typedef uint32_t page_dir_t;
-typedef uint32_t page_table_t;
+typedef struct paget{
+  uint32_t present;
+  uint32_t readwrite;
+  uint32_t usersupervisor;
+  uint32_t writethrough;
+  uint32_t cachedisabled;
+  uint32_t accessed;
+  uint32_t dirty;
+  uint32_t pat;
+  uint32_t global;
+  uint32_t available;
+  uint32_t pbadress;
+} page_t;
+
+typedef struct page_table {
+   page_t pages[1024];
+} page_table_t;
+
+typedef struct page_dir {
+  page_table_t *tables[1024];
+  uint32_t tablesPhysical[1024];
+  uint32_t pysicalAdress;
+} page_directory_t;

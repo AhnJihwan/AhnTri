5db7a1dbcd1fd74a5526e6fa51f3419877ee6852
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-09-02 10:38:19 +0900

    Update paging.c

--- mm/paging.c
+++ mm/paging.c
@@ -1,4 +1,20 @@
 #include "../libc/atclib.h"
 #include "paging.h"
 
-//Paging will be implemented here
+page_table_t page_table_i[1024];
+
+int init_page_table(page_table_t page_table){
+  int i;
+  for(i=0; i<1024; i++){
+    page_table[i].present = 1;
+    page_table[i].readwrite = 1;
+    page_table[i].usersupervisor = 1;
+    page_table[i].writethrough = 1;
+    page_table[i].cachedisabled = 0;
+    page_table[i].accessed = 0;
+    page_table[i].dirty = 0;
+    page_table[i].dirty = 0;
+    page_table[i].pat = 0;
+    page_table[i].global = 0;
+  }
+}

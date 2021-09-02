d1be6d018e432417bd2647f5aa9c6a8ef9cf7e99
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-09-02 11:13:33 +0900

    Update paging.c

--- mm/paging.c
+++ mm/paging.c
@@ -3,18 +3,18 @@
 
 page_table_t page_table_i[1024];
 
-int init_page_table(page_table_t page_table){
+void init_page_table(page_t *page){
   int i;
   for(i=0; i<1024; i++){
-    page_table[i].present = 1;
-    page_table[i].readwrite = 1;
-    page_table[i].usersupervisor = 1;
-    page_table[i].writethrough = 1;
-    page_table[i].cachedisabled = 0;
-    page_table[i].accessed = 0;
-    page_table[i].dirty = 0;
-    page_table[i].dirty = 0;
-    page_table[i].pat = 0;
-    page_table[i].global = 0;
+    page->present = 1;
+    page_table->readwrite = 1;
+    page_table->usersupervisor = 1;
+    page_table->writethrough = 1;
+    page_table->cachedisabled = 0;
+    page_table->accessed = 0;
+    page_table->dirty = 0;
+    page_table->dirty = 0;
+    page_table->pat = 0;
+    page_table->global = 0;
   }
 }

8053a358c522790fb30419c163bc546e7a87b48c
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-09-10 09:31:42 +0900

    Update paging.c

--- mm/paging.c
+++ mm/paging.c
@@ -1,3 +1,4 @@
+
 #include "../libc/atclib.h"
 #include "paging.h"
 
@@ -37,7 +38,7 @@ void init_page(page_t page){
 	page.global = 0;
 }
 */
-
+/*
 void init_page(page_t page){
 	int i;
 	page.present = 0;
@@ -64,4 +65,28 @@ void init_page_dir(page_dir_t page_dir){
 	for(i=0;i<1024;i++){
 		init_page(page_dir.pages[i]);
 	}
+}
+*/
+
+#include "../libc/atclib.h"
+#include "paging.h"
+
+
+void clear_page_directory(uint32_t page_dir[]){
+  uint32 i;
+  for(i = 0; i < 1024; i++) {
+    page_dir[i] = 0x00000002;
+  }
+}
+
+
+void clear_page_table(uint32_t page_tble[]){
+  uint32 i;
+  for(i = 0; i < 1024; i++){
+    page_tble[i] = (i * 0x1000) | 3;
+  }
+}
+
+void ptinpd(uint32_t page_directory[], uint32_t page_table[]){
+	page_directory[0] = ((unsigned int)page_table) | 3;
 }

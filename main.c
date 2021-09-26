3db5d3a3432b3105cda2cb28700a3b8472ae4ccb
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-09-26 16:08:13 +0900

    Update main.c

--- main.c
+++ main.c
@@ -172,7 +172,8 @@ void mkern_main(multiboot_info_t* multiboot)
   asm volatile("\tint $0");
   irq_install();
   extern uint8_t *_kernel_end;								//Defined in Linker.ld
-  pmm_init((uint32_t) &_kernel_end, mem_size);
+  uint32_t sizeofpmminit = mb_info->mem_upper + 1024;
+  pmm_init((uint32_t) &_kernel_end, sizeofpmminit);
   qemu_printf_string("Everything is initialized. System is starting...");
   init_tty(multiboot, 0x7fa49d, 0x000000);
   printf_mmap_addr(multiboot);

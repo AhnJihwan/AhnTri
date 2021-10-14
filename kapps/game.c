b4c2e756b48b652c48768fa0f1c4a274150e6295
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-10-14 23:56:06 +0900

    Trashing the old game to a trendy one

--- kapps/game.c
+++ kapps/game.c
@@ -1,36 +1,36 @@
 #include "../kernel.h"
 #include "../libc/atclib.h"
 
-//Catch @Whelk@ Game
-//I know this is all trash, but just...
+//Mugunghwa Ccotchi Pheutseomnida AhnTri ver. Not officially accociated with Squid game or Netflix Inc.(I really enjoyed it, so I made this lol)
+//Whole game revised by Jihwan Ahn, who is a Korean and revised from the original one.
 void game(){
 	char aa;
-	while(1){
-		aa = get_input_keycode();
-		if(aa==KEY_J){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_U){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_H){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_F){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_D){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_W){
-			printf("One @ caught");
-			break;
-		}else if(aa==KEY_Z){
-			printf("One @ caught");
-			break;
+	char bb;
+	clscr();
+	printf("GAME RULES: PRESS A TO GO FORWARD. FIRST, CLICK A, AND YOU SHOULD CLICK A AND B ONCE. E.G.) A, B, A, B... IF YOU MOVE OTHER WAY, YOU WILL DIE.\n");
+	printf("AHNTRI MUGUNGHWA CLASSIC VER 1.0\n");
+	gotoxy(5, 3);
+	printf("____________START LINE_______________\n");
+	gotoxy(25, 5);
+	printf("YOU\n");
+	for(uint32_t i=6; i<24; i++) {
+	aa = get_input_keycode();
+		if(i==9 || i==14 || i==19){
+			printf("MUGUNGHWA HAS BLOOMED");
+			suspend(10);
+		}else if(i<30 && i%2==0 && aa==KEY_A){
+			gotoxy(25, i);
+			printf("YOU");
+			suspend(4);
+		}else if(i<30 && i%2==1 && aa==KEY_B){
+			gotoxy(25, i);
+			printf("YOU");
+			suspend(4);
 		}else{
-			printf("KILLED ALL");
-			break;
+			printf("YOU R DEAD. \\=={}=== - - @ >O\n");
+			suspend(20);
+			asm volatile("hlt");
 		}
 	}
+	printf("U SURVIVED.\n");
 }

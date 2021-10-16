1306347011646987064c0616315d06bc207dd96c
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-10-16 22:18:34 +0900

    Create initrd.c

---
+++ fs/initrd.c
@@ -0,0 +1,9 @@
+//------------------------------
+#define FILE_NAME_LEN 100
+#define OWNER_NUMERIC_ID 8
+#define GROUP_NUMERIC_ID 8
+#define FILESIZE_LEN 12
+#define SECTOR_SIZE 512
+#define OWNER_USER_NAME_SIZE 32
+#define OWNER_GROUP_NAME_SIZE 32
+//-------------------------------

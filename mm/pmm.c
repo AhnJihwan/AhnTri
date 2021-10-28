b709cf88bfd53ae0a4bcdc3f90d9b4b052918f4c
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-10-28 13:45:45 +0900

    Update pmm.c

--- mm/pmm.c
+++ mm/pmm.c
@@ -96,10 +96,14 @@ void pmm_kernel_deinit(void){
     uint32_t pmm_siz_aligned = pmmap_size;
     pmm_region_deinit((uint32_t)&_kernel_end, pmmap_size);
 }
-/*
+
 void *pmallocblock(){
-	if(used_blocks - max_blocks =< 0){
+	if(used_blocks - max_blocks =< 0 || bitmap_first_unset(pmmap, max_blocks)==-1){
 		return NULL;
 	}
-	bitmap_set(
-	*/
+	bitmap_set(pmmap, bitmap_first_unset(pmmap, max_blocks));
+	used_blocks++;
+	
+	return (void*)(BLOCK_SIZE*bitmap_first_unset(pmmap, max_blocks));
+}
+	

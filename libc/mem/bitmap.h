c3940e2385474ea3064bd4edc65c2b794cf79485
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-10-28 12:41:53 +0900

    Update bitmap.h

--- libc/mem/bitmap.h
+++ libc/mem/bitmap.h
@@ -17,3 +17,27 @@ void bitmap_set(uint32_t *bitmap, int bit) {
 void bitmap_unset(uint32_t *bitmap, int bit) {
     bitmap[bit / BITMAP_ROW_SIZE] &= ~(1 << (bit % BITMAP_ROW_SIZE));
 }
+/*
+// The following (int) function is borrowed from (lucic71/lucicOS
+) licenced under (BSD 2-Clause "Simplified" License)
+int bitmap_first_unset(uint32_t *bitmap, size_t size) {
+    uint32_t rem_bits = size % 32;
+    for (uint32_t i = 0; i < size / 32; i++){
+        if (bitmap[i] != 0XFFFFFFFF){
+            for (int j = 0; j < 32; j++){
+                if (!(bitmap[i] & (1 << j))){
+                    return (i * 32) + j;
+                }
+            }
+        }
+    }
+    if (rem_bits) {
+        for (uint32_t j = 0; j < rem_bits; j++){
+            if (!(bitmap[size / 32] & (1 << j))){
+                    return ((size / 32) * 32) + j;
+            }
+        }
+    }
+    return -1;
+}
+*/

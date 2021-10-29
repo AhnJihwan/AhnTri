94b4029b440fcf0d9c6616bc970a9ad8c90bd2e4
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-10-29 14:25:49 +0900

    Update pmm.h

--- mm/pmm.h
+++ mm/pmm.h
@@ -1,5 +1,5 @@
-static uint8_t   mem_size;    //Available memory in kilobytes
-static uint32_t used_blocks;  //How much blocks are used/4096 blocks?
-static uint32_t max_blocks;   //Maximum of used blocks/4096
-static uint32_t *pmmap;       //Pysical memory map contaning state of the bitmap 0(free) or 1(used) of each block.
-static uint8_t   pmmap_size;  //The size of the bitmap
+uint8_t   mem_size;    //Available memory in kilobytes
+uint32_t used_blocks;  //How much blocks are used/4096 blocks?
+uint32_t max_blocks;   //Maximum of used blocks/4096
+uint32_t *pmmap;       //Pysical memory map contaning state of the bitmap 0(free) or 1(used) of each block.
+uint8_t   pmmap_size;  //The size of the bitmap

643fb0392a962a095fa80946462501cd79401f0c
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-02 18:30:25 +0900

    Update ccalcgraph.c

--- kapps/ccalcgraph.c
+++ kapps/ccalcgraph.c
@@ -1,1 +1,10 @@
+void ccalcgraph(){
+  framebuffer_clscr(0xffffcc);
+  gotoxy(X, Y); //This is gonna be the middle
 
+  for(i=1; i<max; i++){ //max is going to be the maximum
+    framebuffer_putpixel(X, i);
+    framebuffer_putpixel(i, Y));
+  }
+  
+}

1ec64f16e0ca4456ff637525b30aba326c60df77
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-04 23:18:15 +0900

    Update ccalcgraph.c

--- kapps/ccalcgraph.c
+++ kapps/ccalcgraph.c
@@ -1,10 +1,21 @@
+void graph_putpixel(uint32_t x, uint32_t y, uint32_t c){
+	uint32_t graphxx=203+x;
+	uint32_t graphyy=203-y;
+	if(c==NULL){
+		c=0x7fa49d;
+	}
+	framebuffer_putpixel(graphxx, graphyy, c);
+}
+
 void ccalcgraph(){
   framebuffer_clscr(0xffffcc);
-  gotoxy(X, Y); //This is gonna be the middle
-
-  for(i=1; i<max; i++){ //max is going to be the maximum
-    framebuffer_putpixel(X, i);
-    framebuffer_putpixel(i, Y));
+  for(int i=3; i<400; i++){
+    framebuffer_putpixel(203, i, 0x7fa49d);
+    framebuffer_putpixel(i, 203, 0x7fa49d);
   }
-  
+  gotoxy(3, 420);
+  //TODO: printf~~~~~~~~~~~~
+  //TODO: select one of the graphs
+  //TODO: draw the graphs
+  //TODO: ETC ETC...........
 }

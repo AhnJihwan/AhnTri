2f5b459e5b3d9bcaa4b4d6f7acc3a8866d38128e
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-06 18:47:29 +0900

    Update ccalcgraph.c

--- kapps/ccalcgraph.c
+++ kapps/ccalcgraph.c
@@ -8,6 +8,9 @@ ermission. The use of trademarks 'CCALC' and 'CCALC for AhnTri' is prohibited wi
 righer's permission. Liability and Warranty, as with trademark use, are limited. 
 */
 
+#include "../kernel.h"
+#include "../kinc/api.h"
+
 void graph_putpixel(uint32_t x, uint32_t y, uint32_t c){
 	uint32_t graphxx=203+x;
 	uint32_t graphyy=203-y;
@@ -24,10 +27,10 @@ void ccalcgraph(){
                 framebuffer_putpixel(203, i, 0x7fa49d);
                 framebuffer_putpixel(i, 203, 0x7fa49d);
         }
-        gotoxy(3, 420);
-        printf(" [1] LINEAR FUNCTION\n [2] QUAD. FUNCTION\n [3] CUBIC FUNCTION\n [4] DERIVATIVE OF A QUAD. FUNCTION\n [5] DERIVATIVE OF A CUBIC FUNTION\n [6] COPYRIGHT INFO");
+        printf(" \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n [1] LINEAR FUNCTION\n [2] QUAD. FUNCTION\n [3] CUBIC FUNCTION\n [4] DERIVATIVE OF A QUAD. FUNCTION\n [5] DERIVATIVE OF A CUBIC FUNTION\n [6] COPYRIGHT INFO");
         int inputgrph;
-        inputgraph = read_int();
+        inputgrph = read_int();
+        suspend(1);
         int a, b, c, d;
         printf("Your Input, input 0 if not be going to be used. y=ax+b, y=ax^2+bx+c, y=ax^3+bx^2+cx+d\nYour a:");
         a = read_int();
@@ -37,22 +40,25 @@ void ccalcgraph(){
         c = read_int();
         printf("\nYour d: ");
         d = read_int();
-        for(k = -203, k<204, k++){
-                if(inputgrph==1){
-                        graph_putpixel(i, a*k+b, 0x87aaa0);
-		} else if(inputgrph==2){
-			graph_putpixel(i, a*xpy(k, 2)+b*k+c, 0xbfbccb);
-		} else if(inputgrph==3){
-			graph_putpixel(i, a*xpy(k, 3)+b*xpy(k, 2)+c*k+d, 0x16a26c);
-		} else if(inputgrph==4){
-			graph_putpixel(i, 2*a*k+b, 0xbfbccb);
-		} else if(inputgrph==5){
-			graph_putpixel(i, 3*a*xpy(k, 2)+2*b*k, 0x16a26c);
+        if(inputgrph==1){
+        	for(k = -203; k<204; k++){
+                        graph_putpixel(k, a*k+b, 0x87aaa0);
+                  }
+	} else if(inputgrph==2){
+        	for(k = -203; k<204; k++){
+			graph_putpixel(k, a*xpy(k, 2)+b*k+c, 0xbfbccb);
+                  }
+	} else if(inputgrph==3){
+        	for(k = -203; k<204; k++){
+			graph_putpixel(k, a*xpy(k, 3)+b*xpy(k, 2)+c*k+d, 0x16a26c);
+                  }
+	} else if(inputgrph==4){
+        	for(k = -203; k<204; k++){
+			graph_putpixel(k, 2*a*k+b, 0xbfbccb);
+                  }
+	} else if(inputgrph==5){
+        	for(k = -203; k<204; k++){
+			graph_putpixel(k, 3*a*xpy(k, 2)+2*b*k, 0x16a26c);
 		}
-	}
-	if(inputgrph!=1 | inputgrph!=2 | inputgrph!=3 | inputgrph!=4 | inputgrph!=5){
-		printf("Invalid Input\n");
-		printf("Please insert the valid input again.\n");
-		kernmain();
 	}
 }

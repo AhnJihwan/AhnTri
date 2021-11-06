70a74fcad910367ea72876055817feeacc91d987
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-06 12:07:01 +0900

    Update ccalcgraph.c

--- kapps/ccalcgraph.c
+++ kapps/ccalcgraph.c
@@ -8,14 +8,31 @@ void graph_putpixel(uint32_t x, uint32_t y, uint32_t c){
 }
 
 void ccalcgraph(){
-  framebuffer_clscr(0xffffcc);
-  for(int i=3; i<400; i++){
-    framebuffer_putpixel(203, i, 0x7fa49d);
-    framebuffer_putpixel(i, 203, 0x7fa49d);
-  }
-  gotoxy(3, 420);
-  //TODO: printf~~~~~~~~~~~~
-  //TODO: select one of the graphs
-  //TODO: draw the graphs
-  //TODO: ETC ETC...........
+        framebuffer_clscr(0xffffcc);
+        int k;
+        for(int i=3; i<400; i++){
+                framebuffer_putpixel(203, i, 0x7fa49d);
+                framebuffer_putpixel(i, 203, 0x7fa49d);
+        }
+        gotoxy(3, 420);
+        printf(" [1] LINEAR FUNCTION\n [2] QUAD. FUNCTION\n [3] CUBIC FUNCTION\n [4] DERIVATIVE OF A QUAD. FUNCTION\n [5] DERIVATIVE OF A CUBIC FUNTION\n [6] COPYRIGHT INFO");
+        int inputgrph;
+        inputgraph = read_int();
+        int a, b, c, d;
+        printf("Your Input, input 0 if not be going to be used. y=ax+b, y=ax^2+bx+c, y=ax^3+bx^2+cx+d\nYour a:");
+        a = read_int();
+        printf("\nYour b: ");
+        b = read_int();
+        printf("\nYour c: ");
+        c = read_int();
+        printf("\nYour d: ");
+        d = read_int();
+        for(k = -203, k<204, k++){
+                if(inputgrph==1){
+                        graph_putpixel(i, a*k+b, 0x87aaa0);
+		} else if(inputgrph==2){
+			graph_putpixel(i, a*xpy(k, 2)+b*k+c, 0xbfbccb);
+		} else if(inputgrph==3){
+			graph_putpixel(i, a*xpy(k, 3)+b*xpy(k, 2)+c*k+d, 0x16a26c);
+		} else if(
 }

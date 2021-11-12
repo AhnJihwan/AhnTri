d8363b0aae8d52fbe4ac9032484df7e9a39c9bb6
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-12 14:59:00 +0900

    Update msr.c

--- drivers/msr.c
+++ drivers/msr.c
@@ -3,9 +3,9 @@
 #define PENTIRUM4_INTELXEON_IA32_P5_MC_ADDR_MSR 0x0H
 
 void rdmsr(uint32_t addr, uint32_t lo, uint32_t hi){
-  asm volatile("rdmsr" : "=a"(*lo), "=b"(*hi) : "c"(msr));
+  asm volatile("rdmsr" : "=a"(*lo), "=d"(*hi) : "c"(msr));
 }
 
 void wrmsr(uint32_t addr, uint32_t lo, uint32_t hi){
-  asm volatile("wrmsr" : "=a"(*lo), "=b"(*hi) : "c"(msr));
+  asm volatile("wrmsr" : "=a"(*lo), "=d"(*hi) : "c"(msr));
 }

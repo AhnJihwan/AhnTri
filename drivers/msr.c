91ed336121d121fabf23ef4aec206e0ff8bb6cfd
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-11-13 19:07:43 +0900

    Update msr.c

--- drivers/msr.c
+++ drivers/msr.c
@@ -2,7 +2,12 @@
 
 #include "../libc/atclib.h"
 
-#define PENTIRUM4_INTELXEON_IA32_P5_MC_ADDR_MSR 0x0H
+#define PENTIUM4_INTELXEON_IA32_P5_MC_ADDR_MSR 0x0H
+#define PENTIUM4_INTELXEON_IA32_P5_MC_TYPE_MSR 0x1H
+#define PENTIUM4_INTELXEON_IA32_TIME_STAMP_COUNTER_MSR 0x10H
+#define PENTIUM_INTELXEON_IA32_PLATFORM_ID 0x17H
+#define PENTIUM_INTELXEON_IA32_APIC_BASE 0x1BH
+#define PENTIUM_INTELXEON_MSR_EBC_HARD_POWERON 0x2AH
 
 void rdmsr(uint32_t addr, uint32_t lo, uint32_t hi){
   asm volatile("rdmsr" : "=a"(lo), "=d"(hi) : "c"(addr));

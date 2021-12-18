a0284a1a428f7d5a6c02fb0e181cd834286c4e5b
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-12-18 18:48:27 +0900

    Update advset.c

--- kapps/advset.c
+++ kapps/advset.c
@@ -20,8 +20,10 @@ void advset(){
 	printf("a. Change Theme\n");
 	printf("b. Change Language\n");
 	printf("c. Physical Memory Manager\n");
-	printf("d. Earth Save Mode(Eco Mode)\n");
-	printf("e. Safe Termperature mode\n");
+	printf("d. Earth Save Mode(Eco Mode)(O)\n");
+	printf("e. Earth Save Mode(Eco Mode)(X)\n");
+	printf("f. Safe Termperature mode(O)\n");
+	printf("g. Safe Termperature mode(X)\n");
 	printf("AhnTroid AI is a part of AhnTri and is protected by the Apache 2.0 open source licence.\n");
 	cps = get_input_keycode();
 	if(cps == KEY_3){
@@ -66,11 +68,17 @@ void advset(){
 			printf("Input Not Vaild.\n");
 		}
 	}else if(cps == KEY_D){
-		ia32_energy_pref_status_f();
+		ia32_energy_pref_status_f(true);
 	}else if(cps == KEY_E){
+		ia32_energy_pref_status_f(false);
+	}else if(cps == KEY_F){
 		ia32_therm_interrupt_f(0, true);
 		ia32_therm_interrupt_f(1, true);
 		ia32_therm_interrupt_f(2, true);
+	}else if(cps == KEY_G){
+		ia32_therm_interrupt_f(0, false);
+		ia32_therm_interrupt_f(1, false);
+		ia32_therm_interrupt_f(2, false);
 	} else{
 		clscr();
 		return;

6c4d62ef4603b79e96e85b5d0d7dd8eed4778964
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-12-27 23:23:56 +0900

    Create acpi.h

---
+++ drivers/acpi.h
@@ -0,0 +1,12 @@
+typedef struct rsdp {
+  uint8_t sign;
+  uint8_t csum;
+  uint8_t oemid;
+  uint8_t revision;
+  uint8_t rsdtaddr;
+  uint8_t len;
+  uint8_t xsdtaddr;
+  uint8_t xcsum;
+  uint8_t reserved;
+}
+

e357347852632fc9125a2fd8ee66e0d22120233e
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-12-28 09:15:00 +0900

    Update acpi.h

--- drivers/acpi.h
+++ drivers/acpi.h
@@ -10,3 +10,16 @@ typedef struct rsdp {
   uint8_t reserved;
 } rsdp_t;
 
+typedef struct rsdt {
+  uint8_t sign;
+  uint8_t len;
+  uint8_t revision;
+  uint8_t csum;
+  uint8_t oemid;
+  uint8_t oemtbleid;
+  uint8_t oemrevision;
+  uint8_t creatorid;
+  uint8_t creatorrevision;
+  uint32_t entry;
+} rsdt_t;
+

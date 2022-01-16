9a506e5ccae492f720f8fa59225857210c9096b7
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-01-16 21:20:24 +0900

    Update stdint.h

--- kapps/stdint.h
+++ kapps/stdint.h
@@ -5,16 +5,3 @@ int strcmp(char s1[], char s2[]) {
     }
     return s1[i] - s2[i];
 }
-
-int strncmp(char *s1, char *s2, uint8_t n){
-    while(n && *s1 && (*s1 == *s2)){
-        s1++;
-        s2++;
-        n--;
-    }
-    if(n==0){
-        return 0;
-    }else{
-        return 1;
-    }
-}

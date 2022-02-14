3313e5a621123176212bdb300c4dbc21d56a37bb
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-02-15 00:28:19 +0900

    Create scicalc.c

---
+++ kapps/scicalc.c
@@ -0,0 +1,23 @@
+#define H 1
+#define He 2
+#define Li 3
+#define Be 4
+#define B 5
+#define C 6
+#define N 7
+#define O 8
+#define F 9
+#define Ne 10
+#define Na 11
+#define Mg 12
+#define Al 13
+#define Si 14
+#define P 15
+#define S 16
+#define Cl 17
+#define Ar 18
+#define K 19
+#define Ca 20
+
+#define molcweight(x) (x * 2)
+//Never use this to H... H is still 1

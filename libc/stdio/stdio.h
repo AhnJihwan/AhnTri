55bef7823bbec07b50b204c7558ca080de312244
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-02-17 00:55:40 +0900

    inw is a 16 inw는 16입니다.

--- libc/stdio/stdio.h
+++ libc/stdio/stdio.h
@@ -14,7 +14,7 @@ void outb(uint16 port, uint8 data)
   asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
 }
 
-uint8 inw(uint16 port)
+uint16 inw(uint16 port)
 {
   uint16 data;
   asm volatile("inw %1, %0" : "=a"(data) : "Nd"(port));

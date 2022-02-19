883f9d9ded8ddeae40e3c3c19be0abd6d892dcd3
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-02-19 18:57:17 +0900

    Enable ACPI Command ACPI 작동 커멘드

--- drivers/acpi.c
+++ drivers/acpi.c
@@ -139,7 +139,8 @@ void parse_dsdt(acpi_header_t* head){
 int enable_acpi(){
 	if((inw((unsigned int) PM1a_CNT_BLK) &SCI_EN) == 0){
 		if (smi_cmd != 0 && acpi_enable != 0){
-		
+			//Enable ACPI
+			outb((unsigned int) SMI_CMD, ACPI_ENABLE);
 		}else{
 			printf("ACPI can't be enabled...");
 		}

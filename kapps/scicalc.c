17987398224404c23a505a9ad377022873acd63e
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-02-24 18:17:29 +0900

    Update scicalc.c

--- kapps/scicalc.c
+++ kapps/scicalc.c
@@ -19,5 +19,15 @@
 #define K 19
 #define Ca 20
 
-#define molcweight(x) (x * 2)
+#define molarity(x) (x * 2)
 //Never use this to H... H is still 1
+
+#define NumAvogadroOversimplified 6 //.02*10^23
+
+int findmolweight(int element, int numbers){
+  if(element != H){
+    return molarity(element) * numbers;
+  }else if(element == H){
+    return numbers;
+  }
+}

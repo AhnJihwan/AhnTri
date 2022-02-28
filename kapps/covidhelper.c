1b9f9bd2c82f2b7c6287f65fa27eb501c29ac936
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-02-28 22:19:59 +0900

    COVID Helper 코로나바이러스 도움이

--- kapps/covidhelper.c
+++ kapps/covidhelper.c
@@ -2,5 +2,26 @@
 #include "../libc/atclib.h"
 
 void covidhelper(int temp, int spo2){
-  //Stuff here
+  if(temp <= 94){
+    printf("Normal Temperature\n");
+  } else{
+    printf("Might be infected. Take the COVID test!\n");
+  }
+  if(spo2 >= 94){
+    printf("SpO2 Normal\n");
+  }else if(spo2 < 94 && spo2 >= 90){
+    printf("SpO2 Not bad...\n");
+  }else{
+    printf("Might be dangerous.\n");
+  }
+}
+
+void covidhelperkapp(){
+  int temp_covid, spo2_covid;
+  printf("Your Temperature(*F): ");
+  temp_covid = read_int();
+  printf("SpO2 number given: ");
+  spo2_covid = read_int();
+  covidhelper(temp_covid, spo2_covid);
+  printf("If you use celcius, consider that normal temp of C is 36~37 degrees celcius.\n");
 }

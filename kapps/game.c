9ceafb6cae6d9141f6c9b724c4e6c20d26ad9135
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-03-20 03:19:25 +0900

    Number Guess Game 숫자맞추기게임

--- kapps/game.c
+++ kapps/game.c
@@ -5,6 +5,7 @@ void game(){
 	int i;
 	i = (int)xor32() % 10;
 	int ii;
+	printf("Guess a natural number in the set X(X={x|0<=x<=10}) ... ");
 	ii = read_int();
 	if(i = ii){
 		printf("The number picked is right!");

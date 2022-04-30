bf62d207612c1853725de2ae6af305e7fc607e6f
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2022-04-30 17:41:51 +0900

    Update random.h

--- libc/random/random.h
+++ libc/random/random.h
@@ -1,5 +1,5 @@
 unsigned int xor32(){
-  static unsigned int l = 7766UL;
+  static unsigned int l = 77666051UL;
   l ^= l << 17;
   l ^= l >> 12;
   return l ^= l << 19;
@@ -9,8 +9,8 @@ unsigned int xor32_2(){
   static unsigned int l = 37242598UL;
   l ^= l << 55;
   l ^= l >> 23;
-  l ^= l >> 822;
-  l ^= l << 556;
+  l ^= l >> 68;
+  l ^= l << 81;
   return l ^= l << 16;
 }
 
